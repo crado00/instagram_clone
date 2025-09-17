65509f9582adaf207cde46b649caa713f5a01b65
crado00 <alrlawowns@gmail.com>
2025-09-17 15:50:42 +0900

    Sign Up commit

---
+++ backend/src/main/java/com/example/backend/service/AuthService.java
@@ -0,0 +1,14 @@
+package com.example.backend.service;
+
+import com.example.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+}
